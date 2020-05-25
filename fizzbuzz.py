3c4fb9ba994af40f55bd1586f2e398d85d83cb4c
yigiozer <yigitozer05@yahoo.com>
2020-05-25 20:56:11 +0300

    ilk

---
+++ fizzbuzz.py
@@ -0,0 +1,86 @@
+for i in range(1, 101):
+
+    if i % 3 == 0 and i % 5 == 0:
+        print("FIZZBUZZ")
+    elif i % 5 == 0:
+        print("BUZZ")
+    elif i % 3 == 0 :
+        print("FIZZ")
+    else:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
